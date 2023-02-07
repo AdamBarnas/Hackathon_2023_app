97ebdaa13040e2975dd4d2779795f5e35169bb46
Mateusz Cierpik <mateuszcierpik@gmail.com>
2023-02-07 21:33:41 +0100

    rozpoznawanie dłoń zamknięta vs otwarta

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import cv2
+import hand_detection
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    rece = hand_detection.HandDetection()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        success, image = cap.read()
+
+        rece.hand_detecting(image)
+
+        cv2.imshow('MediaPipe Hands', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+    cap.release()
